3c629ab66872f7567dd35c3f36c241ed7eaf9dfe
Stheeven Quishpe <stheeven.quishpe@udla.edu.ec>
2023-10-17 09:29:43 -0500

    Se incluyo un if y else para poder hacer la secuencia

--- Ejercicio39.c
+++ Ejercicio39.c
@@ -3,15 +3,25 @@ int main(){
 
 int num;
 int aux1= 0;
-int aux2= 0;
+int aux2= 1;
 int aux3;
 
 
-printf("Coloque un numero que se o el 1 o el 0\n");
+printf("Coloque la cantidad de numero que se debe mostrar de la serie de Fibonacci\n");
 scanf("%d", &num);
 for (int i = 0; i < num; i++)
 {
+    if (i <= 1)
+    {
+        aux3= i;
+    }
+    else {
+        aux3=aux1+aux2;
+        aux1=aux2;
+        aux2=aux3;
+    }
     
+    printf("%d\n", aux3);
 }
 
 
