b4759374ec826e7a0cf166cd16dc45a184c08775
Stheeven Quishpe <stheeven.quishpe@udla.edu.ec>
2023-10-17 09:05:16 -0500

    Se incluyo un printf para aque se pueda ver mejor el resultado y un if else para que no se puedan ingresar numeros negativos

--- Ejercicio30.c
+++ Ejercicio30.c
@@ -5,7 +5,9 @@ int main(){
 int num;
 printf("Ingrese un numero entero\n");
 scanf("%d", &num);
-
+if (num>0)
+{
+printf("Los componentess numericos de %d son:\n", num);
 for (int i = 1; i <= num; i++)
 {
     if (num % i == 0)
@@ -17,3 +19,11 @@ for (int i = 1; i <= num; i++)
 
 
 }
+else   
+{
+    printf("El numero que ingreso es un numero negativo\n");
+}
+}
+
+
+
